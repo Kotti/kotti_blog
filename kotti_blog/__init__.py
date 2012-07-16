752543e7511b975c8b5924f250c7617fea3d01e9
Marco Scheidhuber <marco@jusid.de>
2012-07-16 10:08:30 +0200

    Use fanstatic instead of head slot for CSS.

--- kotti_blog/__init__.py
+++ kotti_blog/__init__.py
@@ -1,5 +1,13 @@
+from fanstatic import Library
+from fanstatic import Resource
 from pyramid.i18n import TranslationStringFactory
+from kotti.static import view_needed
+
 _ = TranslationStringFactory('kotti_blog')
+
+library = Library("kotti_navigation", "static")
+kotti_navigation_css = Resource(library, "style.css")
+view_needed.add(kotti_navigation_css)
 
 
 def kotti_configure(settings):
