12a5800b95082de7d9f45508949abdb3ffc4faa9
Marco Scheidhuber <marco@jusid.de>
2012-07-16 09:47:31 +0200

    Version bump, add CHANGES.txt, update README a little bit.

--- README.rst
+++ README.rst
@@ -1,1 +1,22 @@
 Blog addon for Kotti.
+
+==========
+kotti_blog
+==========
+
+This is an extension to the Kotti CMS that adds a simple blog system your site.
+
+`Find out more about Kotti`_
+
+Setting up kotti_blog
+=====================
+
+This Addon adds two new Content Types to your Kotti site.
+To set up the content types add ``kotti_blog.kotti_configure``
+to the ``kotti.configurators`` setting in your ini file::
+
+    kotti.configurators = kotti_blog.kotti_configure
+
+Now you can add a blog and add blog entries to your blog.
+
+.. _Find out more about Kotti: http://pypi.python.org/pypi/Kotti
--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2dev'
 
 tests_require = [
     'pytest-cov',
     'pytest',
 ]
 
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+long_description = (
+    README
+    + '\n' +
+    CHANGES
+)
+
 setup(name='kotti_blog',
       version=version,
       description="Kotti blog",
-      long_description="""\
-A blog addon for kotti""",
+      long_description=long_description,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='kotti blog',
       author='',
