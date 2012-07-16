d4f9dedec1f628dd19d485b2682335ce5c685edb
Marco Scheidhuber <marco@jusid.de>
2012-07-16 10:05:16 +0200

    Use pyramid.inlucdes instead of kotti.includes.

--- kotti_blog/__init__.py
+++ kotti_blog/__init__.py
@@ -1,3 +1,3 @@
 def kotti_configure(settings):
-    settings['kotti.includes'] += ' kotti_blog.views'
+    settings['pyramid.includes'] += ' kotti_blog.views'
     settings['kotti.available_types'] += ' kotti_blog.resources.Blog kotti_blog.resources.BlogEntry'
