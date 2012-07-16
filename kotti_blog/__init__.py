63b3b27d5bb38585e33ffda336332b3af2e768e0
Marco Scheidhuber <marco@jusid.de>
2012-07-16 10:05:35 +0200

    Add message factory.

--- kotti_blog/__init__.py
+++ kotti_blog/__init__.py
@@ -1,3 +1,7 @@
+from pyramid.i18n import TranslationStringFactory
+_ = TranslationStringFactory('kotti_blog')
+
+
 def kotti_configure(settings):
     settings['pyramid.includes'] += ' kotti_blog.views'
     settings['kotti.available_types'] += ' kotti_blog.resources.Blog kotti_blog.resources.BlogEntry'
