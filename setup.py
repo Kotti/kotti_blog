05566f577701358a5ef8a780456221c9b0dabdf5
Marco Scheidhuber <j23d@jusid.de>
2013-10-20 02:08:59 +0200

    Prepare kotti_blog 0.4.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4dev'
+version = '0.4'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
